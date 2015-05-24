bbd50c582387f089f6a3dd1c7f5dbb01dc9d972f
Kyle Leng <hyle200@gmail.com>
2015-05-24 01:47:33 -0400

    Javascript challenge 1
    Completed everything except noscript.

---
+++ javascript-challenge-1.js
@@ -0,0 +1,7 @@
+function toggleEmail(){
+	if(document.getElementById("subscribe").checked){
+		document.getElementById("emailpara").style.display = "inline"; //inline the display
+	}else{
+		document.getElementById("emailpara").style.display = "none"; //inline the display
+	}
+};
